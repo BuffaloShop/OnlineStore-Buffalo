d8904765f26893918195f4d4ef758e9259ebe324
BuffaloShop <89865874+BuffaloShop@users.noreply.github.com>
2021-08-31 13:31:30 -0500

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,17 @@
+<footer>
+    <h3 class="text-center">Siguenos en</h3><br>
+    <ul class="list-unstyled text-center">
+        <a href="https://www.facebook.com/pg/BuffaloRopaParaHombre/" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Facebook">
+            <img src="assets/icons/social-facebook.png"  alt="facebook-icon">
+        </a>
+
+        <a href="https://www.facebook.com/pg/BuffaloRopaParaHombre/" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="YouTube">
+            <img src="assets/icons/youtube.PNG" alt="youtube-icon">
+        </a>
+        <a href="https://www.facebook.com/pg/BuffaloRopaParaHombre/" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Instagram">
+            <img src="assets/icons/instagram.PNG" alt="instagram-icon">
+        </a>
+    </ul>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo">BuffaloRopaParaHombre &copy; 2021</h5>
+</footer>
